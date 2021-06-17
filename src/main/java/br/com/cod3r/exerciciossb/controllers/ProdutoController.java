094e38fb55a9f4bab120f44ddb7f8777736548ab
wagnersilvafilho <wagnersf97@gmail.com>
2021-06-17 16:23:12 -0300

    Adicionado ao ProdutoController a consulta paginada

--- src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -1,14 +1,16 @@
 package br.com.cod3r.exerciciossb.controllers;
 
+
 import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -37,6 +39,15 @@ public class ProdutoController {
 		return produtoRepository.findAll();
 	}
 	
+	@GetMapping(path = "/pagina/{numeroPagina}/{qtdePagina}")
+	public Iterable<Produto> obterProdutosPorPagina (@PathVariable int numeroPagina, @PathVariable int qtdePagina){
+		if(qtdePagina >= 5) qtdePagina = 5;
+		Pageable page = PageRequest.of(numeroPagina, 2);
+		return produtoRepository.findAll(page);
+	}
+	
+	
+	
 	@GetMapping(path = "/{id}")
 	public Optional<Produto> obterProdutoPorId(@PathVariable Long id){
 		return produtoRepository.findById(id);
@@ -52,6 +63,8 @@ public class ProdutoController {
 	public void excluirProduto(@PathVariable Long id) {
 		produtoRepository.deleteById(id);
 	}
+	
+	
 	
 }
 	
